0628d9eec40ab2e13b4c01c11a01a67e5ce1bdf5
nataliakeller <natalia-farah@uergs.edu.br>
2020-07-04 17:27:15 +0200

    project foodfy done

--- desafios/FoodFy/scripts.js
+++ desafios/FoodFy/scripts.js
@@ -9,44 +9,66 @@ const modal_title = document.querySelector('#modal_title');
 const modal_description = document.querySelector('#modal_description');
 
 for (let recipe of recipes) {
-    let recipeName = recipe.classList[1];
-
     
+    let recipeName = recipe.classList[1];
 
     recipe.addEventListener("click", function() {
         modalOverlay.classList.add('modal_active');
 
         switch (recipeName) {
             case 'pizza':
-                console.log('pizza')
                 modal_image.src = 'img/pizza.png';
+                modal_title.innerHTML = 'Pizza 4 Estações';
+                modal_description.innerHTML = 'Por Fabiana Melo';
+
                 break;
             
             case 'burger':
-                console.log('burger')
+                modal_image.src = 'img/burger.png';
+                modal_title.innerHTML = 'Triplo Bacon Burger';
+                modal_description.innerHTML = ' por Jorge Relato';
+
                 break;
             case 'asinhas':
-                console.log('asinhas')
+                modal_image.src = 'img/asinhas.png';
+                modal_title.innerHTML = 'Asinhas de frango ao barbecue';
+                modal_description.innerHTML = 'por Vania Steroski';
                 break;
             case 'doce':
-                console.log('doce')
+                modal_image.src = 'img/doce.png';
+                modal_title.innerHTML = 'Docinhos pão-do-céu';
+                modal_description.innerHTML = 'por Ricardo Golvea';
                 break;
 
             case 'lasanha':
-                console.log('lasanha')
+                modal_image.src = 'img/lasanha.png';
+                modal_title.innerHTML = 'Lasanha mac n\' cheese';
+                modal_description.innerHTML = 'por Juliana Vieira';
                 break;
 
             case 'espaguete':
-                console.log('espaguete')
+                modal_image.src = 'img/espaguete.png';
+                modal_title.innerHTML = 'Espaguete ao alho';
+                modal_description.innerHTML = 'por Júlia Kinoto';
                 break;
                 
             default:
                 break;
-        }
-
+        };
     });
 };
 
 modal_close.addEventListener("click", function() {
     modalOverlay.classList.remove('modal_active');
 });
+
+modalOverlay.addEventListener("click", function() {
+    modalOverlay.classList.remove('modal_active');
+});
+
+window.onclick = function(event) {
+    if (event.target == modalOverlay) {
+        modalOverlay.classList.remove('modal_active');
+    };
+};
+
