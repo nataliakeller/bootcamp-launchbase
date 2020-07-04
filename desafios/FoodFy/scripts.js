16363f85bd99e5bf23a36de70fc625dec1618ba3
nataliakeller <natalia-farah@uergs.edu.br>
2020-07-04 16:31:15 +0200

    scripts.js recipeName almost done

--- desafios/FoodFy/scripts.js
+++ desafios/FoodFy/scripts.js
@@ -4,10 +4,45 @@ const modal_close = modalOverlay.querySelector('.modal_close p');
 const node_recipes = document.querySelectorAll('.recipe_block');
 const recipes = Array.from(node_recipes);
 
+const modal_image = document.querySelector('#modal_image')
+const modal_title = document.querySelector('#modal_title');
+const modal_description = document.querySelector('#modal_description');
+
 for (let recipe of recipes) {
+    let recipeName = recipe.classList[1];
+
+    
+
     recipe.addEventListener("click", function() {
         modalOverlay.classList.add('modal_active');
-        
+
+        switch (recipeName) {
+            case 'pizza':
+                console.log('pizza')
+                modal_image.src = 'img/pizza.png';
+                break;
+            
+            case 'burger':
+                console.log('burger')
+                break;
+            case 'asinhas':
+                console.log('asinhas')
+                break;
+            case 'doce':
+                console.log('doce')
+                break;
+
+            case 'lasanha':
+                console.log('lasanha')
+                break;
+
+            case 'espaguete':
+                console.log('espaguete')
+                break;
+                
+            default:
+                break;
+        }
 
     });
 };
@@ -15,6 +50,3 @@ for (let recipe of recipes) {
 modal_close.addEventListener("click", function() {
     modalOverlay.classList.remove('modal_active');
 });
-
-
-console.log(recipes)
