a84c6c95c9d78049125333e3c0cfbb882c5b488e
nataliakeller <natalia-farah@uergs.edu.br>
2020-07-11 11:16:33 +0200

    server f03 pre-njk

--- lessons/module_F03/server/server.js
+++ lessons/module_F03/server/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.use(express.static('public'));
+
+server.set('view engine', 'html');
+
+nunjucks.configure('views', {
+    express: server
+});
+
 server.get('/', function (req, res) {
-    return res.send('oi pessoaldsdfgdfgfdsf')
-})
+    return res.render('about');
+});
+
+
+server.get('/content', function (req, res) {
+    return res.render('content');
+});
 
 server.listen(5000, function() {
     console.log('server is running');
