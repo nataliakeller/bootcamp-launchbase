6bebd95e522c8bab1f7d4269a3955854bcd192a0
nataliakeller <natalia-farah@uergs.edu.br>
2020-07-19 17:59:51 +0200

    0402 done

---
+++ challenges/04-02/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+const routes = require('./routes');
+
+server.use(routes);
+server.use(express.static('public'));
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+});
+
+server.listen(5000, function() {
+    console.log('Server is running.');
+});
+
+
+// server.use(function(req, res) {
+//     res.status(404).render("not-found");
+//   });
