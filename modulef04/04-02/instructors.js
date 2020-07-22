011fbde75ebc7bfddda6deec3eaede836e33d377
nataliakeller <natalia-farah@uergs.edu.br>
2020-07-22 22:57:51 +0200

    instructors/create done

--- modulef04/04-02/instructors.js
+++ modulef04/04-02/instructors.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
+const fs = require('fs'); // File System
 const data = require('./data.json');
 
 exports.post = function (req, res) {
-    const keys = Object.keys(req.body); // IMPORTANTE
+    const keys = Object.keys(req.body); // Validação
 
     for (key of keys) {
         
@@ -10,24 +10,29 @@ exports.post = function (req, res) {
             return res.send('Please enter the required fields.');
         };
     };
-    req.body.id = Number(data.instructor.length) + 1
 
-    req.body.birth = Date.parse(req.body.birth); 
-    // Pegando a data de nascimento e transformando no tempo do timestamp
-    req.body.created_at = Date.now(); 
-    // Pegando a informação da data de criação
+    let { avatar_url, name, birth, gender, services, type_class } = req.body; // Desestruturando o req.body
     
-    const { avatar_url, gender } = req.body;
-    console.log(avatar_url, gender);
+    birth = Date.parse(birth); // Pegando a data de nascimento e transformando no tempo do timestamp
+    const created_at = Date.now();  // Pegando a informação da data de criação
+    const id = Number(data.instructors.length) + 1; // Criando ID
+    
+    data.instructors.push({
+        avatar_url,
+        birth,
+        gender,
+        created_at,
+        services,
+        type_class,
+        id,
+        name
+    });  // Empurrando req.body no array do data.json
 
-
-    data.instructors.push(req.body); // Empurrando req.body no array do data.json
-    // escrevendo os dados no arquivo data.json
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) { 
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) { // escrevendo os dados no arquivo data.json
 
         if (err) return res.send("writeFile error!");
 
         return res.redirect('instructors');
     });
-    // return res.send(req.body);
+    
 };
