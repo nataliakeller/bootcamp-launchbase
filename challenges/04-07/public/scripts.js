c42c4d96e8e876fa9681656c59572280dbd30cc5
nataliakeller <natalia-farah@uergs.edu.br>
2020-08-17 18:34:42 +0200

    Ajustes finais

--- challenges/04-07/public/scripts.js
+++ challenges/04-07/public/scripts.js
@@ -1,9 +1,17 @@
 const currentPage = location.pathname;
 const menuItems = document.querySelectorAll('header ul .menu a');
 
+const formDelete =  document.querySelector('#form-delete');
+
+formDelete.addEventListener('submit', function(event) {
+    const confirmation = confirm("Do you want to delete?");
+    if (!confirmation) {
+        event.preventDefault();
+    };
+});
+
 for (item of menuItems) {
     if(currentPage.includes(item.getAttribute('href'))) {
         item.classList.add('active');
     }
 }
-
